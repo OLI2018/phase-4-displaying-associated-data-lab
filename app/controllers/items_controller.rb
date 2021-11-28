8b3049bdf12641e8fdec8a74c1828df3cf38a172
Oleg <roxvemm2014@gmail.com>
2021-11-28 11:04:52 -0800

    commit tree 32b0e5b7bd30c45b4961eda02cdfddbba64945f1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+    def index
+        # item = Item.all.order(rating: :desc)
+        items = Item.all
+        render json: items, include: :user
+    end
 end
